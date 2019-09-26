497c4b4926e04b13b8478e3b2b01d474b16cbc21
C820596 <C820596@CIBL226820>
2019-09-26 17:08:55 +0200

    first commit

---
+++ API/Controllers/WeatherController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using API.Models;
+using Newtonsoft.Json;
+
+namespace API.Controllers
+{
+    public class WeatherController : Controller
+    {
+        // GET: Weather
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async System.Threading.Tasks.Task<IActionResult> GetWeather(string city)
+        {
+            IActionResult _result = new ObjectResult(false);
+            GenericResult _authenticationResult = null;
+            var Weather = new object();
+            try
+            {
+                string url = "http://api.openweathermap.org/data/2.5/weather?q=";
+                string units_appid = "&units=metric&appid=301955a589d76e0addaa307593cc81ab";
+                string uri = url + city + units_appid;
+                var httpClient = new HttpClient();
+                var response = await httpClient.GetAsync(uri);
+
+                //will throw an exception if not successful
+                if (response != null)
+                {
+                    response.EnsureSuccessStatusCode();
+                    string content = await response.Content.ReadAsStringAsync();
+
+                    dynamic dynJson = JsonConvert.DeserializeObject(content);
+
+                    Weather = dynJson;
+
+                    _result = new ObjectResult(Weather);
+                }
+                else if (response == null)
+                {
+                    _authenticationResult = new GenericResult()
+                    {
+                        Succeeded = false,
+                        Message = "failed",
+
+                    };
+                    _result = new ObjectResult(_authenticationResult);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _authenticationResult = new GenericResult()
+                {
+                    Succeeded = false,
+                    Message = ex.Message
+                };
+
+            }
+
+            return _result;
+        }
+    }
+
+    internal class ObjectResult : IActionResult
+    {
+        private bool v;
+
+        public ObjectResult(bool v)
+        {
+            this.v = v;
+        }
+    }
+}
---
+++ API/Models/GenericResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class GenericResult
+    {
+        private object weather;
+
+        public GenericResult(object weather)
+        {
+            this.weather = weather;
+        }
+
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+    }
+}
